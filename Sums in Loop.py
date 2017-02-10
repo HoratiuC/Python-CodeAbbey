78ea468d7e3bab6d31d2cae8b8ca28a121fad08f
HoratiuC <horatiucovrig@gmail.com>
2017-02-10 10:52:21 -0700

    Update Sums in Loop.py

--- Sums in Loop.py
+++ Sums in Loop.py
@@ -1,15 +1,20 @@
+#read the file which contains the pairs
 f=open("C:\\Users\\HoratiuC\\Documents\\numbers.txt","r")
 nums = f.read().split('\n')
 
+#read the first element of the list, showing the number of pairs
 print ("This program sums {} pairs of numbers".format(int(nums[0])))
 
+#remove first element, only pairs remain
 l = nums[1:]
 
+#split the list
 k=[]
 for i in l:
     k.append(i.split())
 
 sum=0
+#sum of each pair will be appended to answer
 answer=[]
 
 for x in range(0,len(k)):
